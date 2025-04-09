b41b9278593207529e006ea922497bf6c98ab725
Heorhii Nadiradze <swain.volt@gmail.com>
2025-04-09 20:34:00 +0300

    add additional function in Base class

--- src/database/models/base.py
+++ src/database/models/base.py
@@ -1,7 +1,17 @@
+from typing import Annotated
+
+from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase
 
 
+str_255 = Annotated[str, 255]
+
+
 class Base(DeclarativeBase):
+    type_annotation_map = {
+        str_255: String(255)
+    }
+
     @classmethod
     def default_order_by(cls):
         return None
