a24b0345ff597febdf03974eb48e5188aa8bae21
Heorhii Nadiradze <swain.volt@gmail.com>
2025-04-12 09:58:47 +0300

    add py file for schemas with pydantic completed classes for register

---
+++ src/schemas/accounts.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+
+
+class UserCreateRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserCreateResponse(UserCreate):
+    id: int
+
