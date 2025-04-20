293da8791cf58d282004daebf268d5fb701b30fc
Heorhii Nadiradze <swain.volt@gmail.com>
2025-04-20 09:19:29 +0300

    add managing user tokens and authenticate

--- src/security/token_manipulation.py
+++ src/security/token_manipulation.py
@@ -4,14 +4,31 @@ from typing import Annotated
 import jwt
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from jwt import InvalidTokenError
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
+from src.database.models.accounts import UserModel
+from src.database.session_sqlite import get_db
+from src.schemas.accounts import UserCreate
 from src.security.validations import verify_password
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 REFRESH_TOKEN_EXPIRE_MINUTES = 30 * 24 * 7
+
+
+fake_users_db = {
+    "_hashed_password": "$2b$12$iEcfZbwkgq.k4YjNtcUrE.dqe.iNT9DFLzfI86JawWAUji3R7B/fG",
+    "is_active": True,
+    "updated_at": "2025-04-14T10:05:48",
+    "id": 1,
+    "email": "user1@example.com",
+    "created_at": "2025-04-14T10:05:11",
+    "group_id": 1
+}
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
@@ -36,33 +53,53 @@ def create_refresh_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 
-
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-# async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         username = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#         token_data = TokenData(username=username)
-#     except InvalidTokenError:
-#         raise credentials_exception
-#     user = get_user(fake_users_db, username=token_data.username)
-#     if user is None:
-#         raise credentials_exception
-#     return user
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/accounts/login/")
 
 
-# async def get_current_active_user(
-#     current_user: Annotated[User, Depends(get_current_user)],
-# ):
-#     if current_user.disabled:
-#         raise HTTPException(status_code=400, detail="Inactive user")
-#     return current_user
+async def get_user_token(token: str = Depends(oauth2_scheme)):
+    return token
+
+
+async def get_user_by_id(user_id, db: AsyncSession):
+    stmt = select(UserModel).where(UserModel.id == user_id)
+    result = await db.execute(stmt)
+    return result.scalar_one_or_none()
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)],
+                           db: AsyncSession = Depends(get_db)
+                           ):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload.get("sub")
+        print(user_id)
+        if user_id is None:
+            raise credentials_exception
+    except InvalidTokenError:
+        raise credentials_exception
+    user = await get_user_by_id(user_id, db)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[UserCreate, Depends(get_current_user)],
+):
+    if not current_user.is_activ:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+async def authenticate_user(user_id: str, password: str, db: AsyncSession = Depends(get_db)):
+    user = await get_user_by_id(user_id, db)
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return user
