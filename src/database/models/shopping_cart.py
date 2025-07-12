f05e32820e433ca1c01d4eecf0a663c298be659d
Heorhii Nadiradze <swain.volt@gmail.com>
2025-07-12 15:54:53 +0300

    fix and change NotificationModeratorsModel (notifications_delete -> notifications_delete_id)

--- src/database/models/shopping_cart.py
+++ src/database/models/shopping_cart.py
@@ -1,13 +1,20 @@
 from datetime import datetime
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
-from sqlalchemy import Integer, ForeignKey, DateTime, func, UniqueConstraint
+from sqlalchemy import Integer, ForeignKey, DateTime, func, UniqueConstraint, Table, Column
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from src.database.models.base import Base
 
 if TYPE_CHECKING:
     from .accounts import UserModel
     from .movies import Movie
+
+NotificationModeratorsModel = Table(
+    "notification_moderators",
+    Base.metadata,
+    Column("user_id", ForeignKey("users.id"), primary_key=True),
+    Column("notifications_delete_id", ForeignKey("notifications_delete.id"), primary_key=True, nullable=False),
+)
 
 
 class CartModel(Base):
@@ -28,5 +35,27 @@ class CartItemsModel(Base):
     movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id", ondelete="CASCADE"), nullable=False)
     movie: Mapped["Movie"] = relationship("Movie", back_populates="cart_items")
     added_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    notifications_delete: Mapped[List["NotificationDeleteModel"]] = relationship(
+        "NotificationDeleteModel",
+        back_populates="cart_items"
+    )
 
     __table_args__ = (UniqueConstraint("cart_id", "movie_id"),)
+
+
+class NotificationDeleteModel(Base):
+    __tablename__ = "notifications_delete"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    cart_items_id: Mapped[int] = mapped_column(ForeignKey("cart_items.id", ondelete="CASCADE"), nullable=False)
+
+    comment: Mapped[str] = mapped_column()
+    users: Mapped[List["UserModel"]] = relationship(
+        "UserModel",
+        secondary=NotificationModeratorsModel,
+        back_populates="notifications_delete"
+    )
+    cart_items: Mapped["CartItemsModel"] = relationship(
+        "CartItemsModel",
+        back_populates="notifications_delete"
+    )
