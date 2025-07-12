6df742586a771606422adc7290ae874dd304a4bf
Heorhii Nadiradze <swain.volt@gmail.com>
2025-07-12 15:52:13 +0300

    add new include for shopping cart to movie.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from src.routes.accounts import router as router_accounts
 from src.routes.movies import router as router_movies
+from src.routes.shopping_carts import router as router_carts
 
 app = FastAPI(
     title="Movies homework",
@@ -12,3 +13,4 @@ prefix_path = "/api/v1"
 
 app.include_router(router_accounts, prefix=f"{prefix_path}/accounts", tags=["accounts"])
 app.include_router(router_movies, prefix=f"{prefix_path}/movies", tags=["movies"])
+app.include_router(router_carts, prefix=f"{prefix_path}/shopping-carts", tags=["carts"])
