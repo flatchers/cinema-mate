6aa4fbb83d8f2593dc5ce0b0d7aa80968e890e73
Heorhii Nadiradze <swain.volt@gmail.com>
2025-07-16 22:40:25 +0300

    add endpoint for deletion order

--- src/routes/orders.py
+++ src/routes/orders.py
@@ -7,7 +7,8 @@ from sqlalchemy.orm import selectinload
 from sqlalchemy.sql.functions import count
 from starlette import status
 
-from src.database.models import UserModel, CartItemsModel, CartModel, OrderModel, OrderItemModel
+from src.database.models import UserModel, CartItemsModel, CartModel, OrderModel, OrderItemModel, Movie
+from src.database.models.order import StatusEnum
 from src.database.session_sqlite import get_db
 from src.schemas.orders import OrderSchemaResponse
 from src.security.token_manipulation import get_current_user
@@ -28,7 +29,12 @@ async def create_order(
     result: Result = await db.execute(stmt)
     cart = result.scalars().first()
 
-    stmt = select(OrderItemModel).join(OrderItemModel.order).options(selectinload(OrderItemModel.movie)).where(OrderModel.user_id == current_user.id)
+    stmt = (
+        select(OrderItemModel)
+        .join(OrderItemModel.order)
+        .options(selectinload(OrderItemModel.movie))
+        .where(OrderModel.user_id == current_user.id)
+    )
     result: Result = await db.execute(stmt)
     order_item_all = result.scalars().all()
 
@@ -102,3 +108,32 @@ async def order_list(
         )
         for order in orders
     ]
+
+
+@router.delete("/delete/{order_id}/")
+async def order_delete(
+        order_id: int,
+        db: AsyncSession = Depends(get_db),
+        current_user: UserModel = Depends(get_current_user)
+):
+
+    stmt = select(UserModel).where(UserModel.id == current_user.id)
+    result: Result = await db.execute(stmt)
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    stmt = select(OrderModel).where(OrderModel.id == order_id, OrderModel.user_id == current_user.id)
+    result = await db.execute(stmt)
+    order = result.scalars().first()
+
+    if not order:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found.")
+
+    if order.status != StatusEnum.PENDING:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Cannot delete order with status: {order.status.value}")
+    order.status = StatusEnum.CANCELED
+    await db.commit()
+
+    return {"response": "order was canceled"}
