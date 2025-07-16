35c1bb89ad788484a1b08dc849140476c07d5cfb
Heorhii Nadiradze <swain.volt@gmail.com>
2025-07-16 17:06:54 +0300

    add new schema for order_list endpoint - OrderSchemaResponse

---
+++ src/schemas/orders.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+from src.database.models.order import StatusEnum
+
+
+class OrderSchemaResponse(BaseModel):
+    created_at: datetime
+    count_films: int
+    total_amount: float
+    status: StatusEnum
