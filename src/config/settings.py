e1abf1a8be93dba6aea4a4eff7835045edc06878
Heorhii Nadiradze <swain.volt@gmail.com>
2025-07-25 18:02:22 +0300

    add Settings model

---
+++ src/config/settings.py
@@ -0,0 +1,14 @@
+import os
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    SENDGRID_API_KEY: str
+
+    STRIPE_PUBLISHABLE_KEY: str
+    STRIPE_SECRET_KEY: str
+    WEBHOOK_ENDPOINT_SECRET: str
+
+    class Config:
+        env_file = ".env"
