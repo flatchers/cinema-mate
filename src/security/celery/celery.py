3ca357b40dd2892f631fbc95669973e2a6e85c63
Heorhii Nadiradze <swain.volt@gmail.com>
2025-08-06 16:15:44 +0300

    fix and change

--- src/security/celery/celery.py
+++ src/security/celery/celery.py
@@ -1,21 +1,17 @@
 import asyncio
 from datetime import datetime, timezone
-import time
-
-from celery import Celery
-from celery.bin import celery
-
 
 from celery import Celery
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.models.accounts import ActivationTokenModel
+from ...database.session_postgresql import get_postgresql_db
 
 celery_app = Celery(
     "cinema_mate",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0"
+    broker="redis://redis:6379/0",
+    backend="redis://redis:6379/0"
 )
 
 
@@ -24,17 +20,18 @@ def delete_tokens():
     asyncio.run(_celery_delete_token())
 
 
-@celery_app.task
-async def _celery_delete_token(db: AsyncSession):
-    now = datetime.now(timezone.utc)
-    stmt = select(ActivationTokenModel).where(ActivationTokenModel.expires_at < now)
-    result = await db.execute(stmt)
-    expired_tokens = result.scalars().all()
-    count = len(expired_tokens)
+async def _celery_delete_token():
+    async with get_postgresql_db() as db:
+        now = datetime.now(timezone.utc)
+        stmt = select(ActivationTokenModel).where(ActivationTokenModel.expires_at < now)
+        result = await db.execute(stmt)
+        expired_tokens = result.scalars().all()
+        count = len(expired_tokens)
 
-    for token in expired_tokens:
-        await db.delete(token)
+        for token in expired_tokens:
+            await db.delete(token)
 
     await db.commit()
     print(f"delete {count} tokens")
 
+from . import celery_configuration
