260b9eb58230ba7e9ebdc1684e0495a3244785bd
Heorhii Nadiradze <swain.volt@gmail.com>
2025-08-19 12:04:17 +0300

    add async tests for creating movie invalid scenarios

--- src/tests/test_integration/test_movies.py
+++ src/tests/test_integration/test_movies.py
@@ -104,10 +104,6 @@ async def test_film_create_invalid_scenarios(db_session, client):
         "password": payload_register["password"]
     }
 
-    stmt = select(UserModel).where(UserModel.email == payload["username"])
-    result: Result = await db_session.execute(stmt)
-    moder = result.scalars().first()
-
     response = await client.post("/api/v1/accounts/login/", data=payload)
     response_data = response.json()
     assert response.status_code == 200
@@ -141,3 +137,62 @@ async def test_film_create_invalid_scenarios(db_session, client):
     )
     response_data = response.json()
     assert "error: (sqlite3.IntegrityError) UNIQUE constraint failed" in response_data["detail"]
+
+
+@pytest.mark.asyncio
+async def test_film_create_invalid_roles(client, db_session):
+    payload_register = {
+        "email": "sample@user.com",
+        "password": "StrongPassword123!"
+    }
+    db_session.add(UserGroup(name=UserGroupEnum.USER))
+    await db_session.flush()
+
+    stmt = select(UserGroup).where(UserGroup.name == UserGroupEnum.USER)
+    result: Result = await db_session.execute(stmt)
+    user_group = result.scalars().first()
+
+    sample_user = UserModel(
+        email=payload_register["email"],
+        password=payload_register["password"],
+        group_id=user_group.id
+    )
+
+    sample_user.is_active = True
+    db_session.add(sample_user)
+    await db_session.commit()
+    assert sample_user.is_active
+
+    payload = {
+        "username": payload_register["email"],
+        "password": payload_register["password"]
+    }
+
+    response = await client.post("/api/v1/accounts/login/", data=payload)
+    response_data = response.json()
+    assert response.status_code == 200
+
+    payload_movie = {
+        "name": "Success Film Test",
+        "year": 2020,
+        "time": 130,
+        "imdb": 6.1,
+        "votes": 100,
+        "meta_score": 10.1,
+        "gross": 9.1,
+        "description": "test for creating film",
+        "price": 10.1,
+        "certification": "test best",
+        "genres": ["drama"],
+        "directors": ["Jo Hoffman"],
+        "stars": ["Alaric Zaltzman"],
+    }
+
+    response = await client.post(
+        "/api/v1/movies/create/",
+        json=payload_movie,
+        headers={"Authorization": f"Bearer {response_data["access_token"]}"}
+    )
+    assert response.status_code == 403, "Expected FOBIDDEN error"
+    response_data = response.json()
+    assert response_data["detail"] == "Access forbidden: insufficient permissions."
