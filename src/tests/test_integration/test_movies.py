de6dab08c889d3e9b43f65b175491f80462d9889
Heorhii Nadiradze <swain.volt@gmail.com>
2025-08-21 20:21:09 +0300

    add async tests for commenting movies endpoint

--- src/tests/test_integration/test_movies.py
+++ src/tests/test_integration/test_movies.py
@@ -1,6 +1,7 @@
 import pytest
 from sqlalchemy import select, Result
 
+from src.database.models.movies import Comment
 from src.database.models.shopping_cart import CartModel, CartItemsModel
 from src.database.models import Movie, PaymentModel, OrderItemModel, OrderModel, UserModel
 from src.database.models.order import StatusEnum
@@ -954,3 +955,76 @@ async def test_add_and_remove_like(client, db_session):
     )
     response_data_like = response.json()
     assert response_data_like["like_count"] == 0
+
+
+@pytest.mark.asyncio
+async def test_write_comments(client, db_session):
+    payload_register = {
+        "email": "testuser@example.com",
+        "password": "StrongPassword123!"
+    }
+
+    db_session.add(UserGroup(name=UserGroupEnum.MODERATOR))
+    await db_session.flush()
+
+    stmt = select(UserGroup).where(UserGroup.name == UserGroupEnum.MODERATOR)
+    result: Result = await db_session.execute(stmt)
+    moderator_group = result.scalars().first()
+
+    moderator = UserModel(
+        email=payload_register["email"],
+        password=payload_register["password"],
+        group_id=moderator_group.id
+    )
+    moderator.is_active = True
+    db_session.add(moderator)
+    await db_session.commit()
+    assert moderator.is_active
+
+    payload = {
+        "username": payload_register["email"],
+        "password": payload_register["password"]
+    }
+
+    response = await client.post("/api/v1/accounts/login/", data=payload)
+    response_data_log = response.json()
+    assert response.status_code == 200
+
+    payload_movie = {
+        "name": "aaa",
+        "year": 2020,
+        "time": 130,
+        "imdb": 6.1,
+        "votes": 100,
+        "meta_score": 10.1,
+        "gross": 9.1,
+        "description": "aaa",
+        "price": 10.1,
+        "certification": "aaaaaaaaa",
+        "genres": ["drama"],
+        "directors": ["Jo Hoffman"],
+        "stars": ["aaa"],
+    }
+    response = await client.post(
+        "/api/v1/movies/create/",
+        json=payload_movie,
+        headers={"Authorization": f"Bearer {response_data_log["access_token"]}"}
+    )
+    assert response.status_code == 201
+    response_data = response.json()
+
+    stmt = select(UserModel).where(UserModel.email == payload_register["email"])
+    result: Result = await db_session.execute(stmt)
+    user = result.scalars().first()
+
+    response = await client.post(
+        f"/api/v1/movies/{response_data["id"]}/comments/",
+        json={"comments": "this film not bad"},
+        headers={"Authorization": f"Bearer {response_data_log["access_token"]}"}
+    )
+    assert response.status_code == 201
+
+    stmt = select(Comment).where(Comment.user_id == user.id)
+    result: Result = await db_session.execute(stmt)
+    comment = result.scalars().first()
+    assert comment
