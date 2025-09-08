6637a5ba7b0f29aeac9a9657072db9da8d189fd0
Heorhii Nadiradze <swain.volt@gmail.com>
2025-09-08 16:01:22 +0300

    style(main): fix flake8 issues in main file

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,10 +1,10 @@
-import pytest
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
-from sqlalchemy import StaticPool
-from sqlalchemy.ext.asyncio import create_async_engine
 
-from src.database.session_sqlite import reset_sqlite_database, get_sqlite_db_contextmanager
+from src.database.session_sqlite import (
+    reset_sqlite_database,
+    get_sqlite_db_contextmanager,
+)
 from src.main import app
 
 
@@ -22,6 +22,8 @@ async def db_session():
 
 @pytest_asyncio.fixture(scope="function", autouse=True)
 async def client(db_session):
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
         yield ac
     app.dependency_overrides.clear()
